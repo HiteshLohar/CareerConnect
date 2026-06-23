c6b2bc4e8475f1494c51e088ba43e638a5359643
HiteshLohar <loharhitesh2002.com>
2026-06-23 16:07:43 +0530

    Add mongoDB connection Configuration

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from "./src/config/db.js";
+dotenv.config();
 
 const app = express();
 
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("Try this now");
 });
 
-app.listen(5000,()=>{
-    console.log("CareerConnect API Running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`CareerConnect API Running on port ${PORT}`);
 })
