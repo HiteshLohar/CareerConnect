2e7e561ed21059cbdbd10f29ecb9fe60aa3824a8
HiteshLohar <loharhitesh2002.com>
2026-06-29 16:30:05 +0530

    Implement user login API

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
-    try{
+    try {
         console.log(req.body);
         const {
             fullName,
@@ -18,19 +18,19 @@ export const register = async (req, res) => {
             location
         } = req.body;
 
-        if(!fullName || !email || !password || !location){
+        if (!fullName || !email || !password || !location) {
             return res.status(400).json({
-                success : false,
-                message : "Please fill all required fields"
+                success: false,
+                message: "Please fill all required fields"
             });
         }
 
-        const existingUser = await User.findOne({email});
-        
-        if(existingUser){
-            res.status(409).json({
-                sucess : false,
-                message : "Email already Exists"
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(409).json({
+                success: false,
+                message: "Email already Exists"
             });
         }
 
@@ -39,7 +39,7 @@ export const register = async (req, res) => {
         const user = await User.create({
             fullName,
             email,
-            password : hashedPassword,
+            password: hashedPassword,
             role,
             phone,
             profilePhoto,
@@ -52,15 +52,52 @@ export const register = async (req, res) => {
 
         user.password = undefined;
 
-        res.status(201).json({
-            success : true,
-            message : "User registered successfully",
+        return res.status(201).json({
+            success: true,
+            message: "User registered successfully",
             user
         });
-    } catch (error){
-        res.status(500).json({
-            success : false,
-            message : error.message
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
         });
     }
 };
+
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Login successful"
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
