9d585462e079c3777906542879a05b4cb54c263f
HiteshLohar <loharhitesh2002@gmail.com>
2026-07-01 15:31:43 +0530

    Add JWT authetication middleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = (req, res, next) => {
+    try{
+        const authHeader = req.headers.authorization;
+
+        if(!authHeader || !authHeader.startsWith("Bearer ")){
+            return res.status(401).json({
+                success : false,
+                message : "Access denied. No token provided."
+            });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decode;
+
+        next();
+    }
+    catch(error){
+        return res.status(401).json({
+            success : false,
+            message : "Invalid or expired token."
+        });
+    }
+}
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controllers.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+
+//demo
+router.get("/profile", verifyToken, (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Protected Route Accessed",
+        user: req.user
+    });
+});
 
 export default router;
