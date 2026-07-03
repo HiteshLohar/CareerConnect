fe17dd8741bd74f9a4804997dc7eb786a4501e35
HiteshLohar <loharhitesh2002@gmail.com>
2026-07-03 13:04:53 +0530

    Implement role-based authorization middleware

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -26,3 +26,17 @@ export const verifyToken = (req, res, next) => {
         });
     }
 }
+
+export const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+
+        if(!roles.includes(req.user.role)){
+            return res.status(403).json({
+                success : false,
+                message : "Access denied. You do not have permission."
+            });
+        }
+
+        next();
+    };
+};
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,12 +1,53 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controllers.js";
-import { verifyToken } from "../middleware/authMiddleware.js";
+import { verifyToken, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 
+
+// this is test to check the role based access control
+router.get("/student", verifyToken, authorizeRoles("student"), (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Welcome Student"
+    });
+}
+);
+
+router.get(
+    "/recruiter",
+    verifyToken,
+    authorizeRoles("recruiter"),
+    (req, res) => {
+        return res.status(200).json({
+            success: true,
+            message: "Welcome Recruiter"
+        });
+    }
+);
+
+router.get(
+    "/admin",
+    verifyToken,
+    authorizeRoles("admin"),
+    (req, res) => {
+        return res.status(200).json({
+            success: true,
+            message: "Welcome Admin"
+        });
+    }
+);
+
+
+
+//this is route only work on recruiter
+// router.post('/create-job', verifyToken, authorizeRoles("recruiter"), createJob);
+
+//this is route only work on recruiter and admin
+// router.get('/all-jobs', verifyToken, authorizeRoles("admin", "recruiter"), getAllJobs);
 
 //demo
 router.get("/profile", verifyToken, (req, res) => {
