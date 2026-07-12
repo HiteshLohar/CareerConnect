8dd4067ebe3a2cd3e7e14f80568471929338779b
HiteshLohar <loharhitesh2002@gmail.com>
2026-07-12 17:28:56 +0530

    Implement company CRUD APIs with ownership validation

--- server/src/controllers/company.controller.js
+++ server/src/controllers/company.controller.js
@@ -1,4 +1,5 @@
 import Company from "../models/Company.js";
+import mongoose from "mongoose";
 
 export const createCompany = async (req, res) => {
     try {
@@ -57,4 +58,178 @@ export const createCompany = async (req, res) => {
     }
 };
 
-//      
+
+export const getAllCompanies = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+
+        const companies = await Company.find({ owner: userId }).sort({ createdAt: -1 });
+
+        if (companies.length === 0) {
+            return res.status(200).json({
+                success: true,
+                message: "No companies found",
+                count: 0,
+                companies: []
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Companies fetched successfully",
+            count: companies.length,
+            companies
+        });
+
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const getCompanyById = async (req, res) => {
+    try {
+        const { id: companyId } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(companyId)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Company ID"
+            });
+        }
+
+        const company = await Company.findById({ _id: companyId });
+
+        if (!company) {
+            return res.status(404).json({
+                success: false,
+                message: "Company not found"
+            });
+        }
+
+        if (req.user.userId !== company.owner.toString()) {
+            return res.status(403).json({
+                success: false,
+                message: "You are not authorized to view this company"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Company fetched successfully",
+            company
+        });
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+export const updateCompany = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const { id: companyId } = req.params;
+
+
+        if (!mongoose.Types.ObjectId.isValid(companyId)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Company ID"
+            });
+        }
+
+        const company = await Company.findById(companyId);
+
+        if (!company) {
+            return res.status(404).json({
+                success: false,
+                message: "Company not found"
+            });
+        }
+
+        if (company.owner.toString() !== userId) {
+            return res.status(403).json({
+                success: false,
+                message: "You are not authorized"
+            });
+        }
+
+        const {
+            name,
+            description,
+            website,
+            location,
+            logo
+        } = req.body;
+
+        // Check duplicate company name only if recruiter wants to change it
+        if (name !== undefined) {
+
+            const existingCompany = await Company.findOne({ name: name.trim() });
+
+            if (
+                existingCompany &&
+                existingCompany._id.toString() !== companyId
+            ) {
+                return res.status(400).json({
+                    success: false,
+                    message: "Company name already exists"
+                });
+            }
+        }
+
+        // Only update fields that are provided
+        const updateData = {};
+
+        if (name !== undefined) {
+            updateData.name = name.trim();
+        }
+
+        if (description !== undefined) {
+            updateData.description = description;
+        }
+
+        if (website !== undefined) {
+            updateData.website = website;
+        }
+
+        if (location !== undefined) {
+            updateData.location = location;
+        }
+
+        if (logo !== undefined) {
+            updateData.logo = logo;
+        }
+
+        const updatedCompany = await Company.findByIdAndUpdate(
+            companyId,
+            updateData,
+            {
+                returnDocument: "after",
+                runValidators: true
+            }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Company updated successfully",
+            company: updatedCompany
+        });
+
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
--- server/src/routes/company.routes.js
+++ server/src/routes/company.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createCompany } from "../controllers/company.controller.js";
+import { createCompany, getAllCompanies, getCompanyById, updateCompany } from "../controllers/company.controller.js";
 import { verifyToken } from "../middleware/authMiddleware.js"
 import { authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", verifyToken, authorizeRoles("recruiter"), createCompany);
+
+router.get("/", verifyToken, authorizeRoles("recruiter"), getAllCompanies);
+
+router.get("/:id", verifyToken, authorizeRoles("recruiter"), getCompanyById);
+
+router.put("/:id", verifyToken, authorizeRoles("recruiter"), updateCompany);
 
 export default router;
