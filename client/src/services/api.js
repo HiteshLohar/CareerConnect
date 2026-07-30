d7ddc805bcf3ddac2f99354be7bfd6bfa1c57ae9
HiteshLohar <loharhitesh2002@gmail.com>
2026-07-30 16:48:46 +0530

    refactor(auth): add axios interceptor and improve auth flow

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,25 @@
 import axios from "axios";
 
+import store from "../redux/store.js";
+import { logout } from "../redux/slices/authSlice.js";
+
 const api = axios.create({
     baseURL: "http://localhost:5000/api",
     withCredentials: true,
 });
+
+api.interceptors.response.use(
+    (response) =>{
+        return response;
+    },
+    (error)=>{
+        if(error.response?.status === 401){
+            store.dispatch(logout());
+
+            console.log("Session expired");
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default api;
