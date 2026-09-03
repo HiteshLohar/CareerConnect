a9afef38c471de98e3d40c1b8667183e1c4e127b
HiteshLohar <loharhitesh2002@gmail.com>
2026-09-03 14:06:00 +0530

    Fix production authentication cookie

--- server/src/controllers/auth.controllers.js
+++ server/src/controllers/auth.controllers.js
@@ -392,7 +392,7 @@ export const login = asyncHandler(async (req, res) => {
             secure:
                 process.env.NODE_ENV === "production",
 
-            sameSite: "lax",
+            sameSite: "none",
 
             maxAge:
                 7 * 24 * 60 * 60 * 1000,
